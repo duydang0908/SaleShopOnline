609ce826ccf61e85613cecb0551184995499699d
duydang0908 <duydang0908@gmail.com>
2018-10-12 14:16:59 +0700

    Product detail(crash)

--- app/src/main/java/com/example/duyda/onlinesaleshop/ProductList.java
+++ app/src/main/java/com/example/duyda/onlinesaleshop/ProductList.java
@@ -110,7 +110,9 @@ public class ProductList extends AppCompatActivity {
                 holder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
-                        Toast.makeText(ProductList.this, "" + clickItem.getName(), Toast.LENGTH_SHORT).show();
+                        Intent productDetail = new Intent(ProductList.this, ProductDetail.class);
+                        productDetail.putExtra("ProductId", adapter.getRef(position).getKey());
+                        startActivity(productDetail);
                     }
                 });
             }
