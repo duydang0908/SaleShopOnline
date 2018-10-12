701ad74a96b5c2278aa8ae44477ce704d9e14be6
duydang0908 <duydang0908@gmail.com>
2018-10-12 15:43:06 +0700

    product detail

---
+++ app/src/main/java/com/example/duyda/onlinesaleshop/LoginActivity.java
@@ -0,0 +1,83 @@
+package com.example.duyda.onlinesaleshop;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.duyda.onlinesaleshop.Common.Common;
+import com.example.duyda.onlinesaleshop.Models.Account;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    Button btnSignIn;
+    MaterialEditText edtPhone, edtPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        edtPhone = findViewById(R.id.edtPhoneNumber);
+        edtPassword =  findViewById(R.id.edtPassword);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_account = database.getReference("Account");
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final ProgressDialog mDialog = new ProgressDialog(LoginActivity.this);
+                mDialog.setMessage("Vui lòng chờ...");
+                mDialog.show();
+
+                table_account.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (edtPhone.getText().toString().isEmpty() || edtPassword.getText().toString().isEmpty()) {
+                            mDialog.dismiss();
+                            Toast.makeText(LoginActivity.this, "Vui lòng điền đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                        } else {
+                            if (dataSnapshot.child(edtPhone.getText().toString()).exists()) {
+                                mDialog.dismiss();
+                                Account account = dataSnapshot.child(edtPhone.getText().toString()).getValue(Account.class);
+                                if (edtPassword.getText().toString().equals(account.getPass())) {
+                                    Intent homeIntent=new Intent(LoginActivity.this,Home.class);
+                                    Common.currentAccount=account;
+                                    startActivity(homeIntent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Lỗi đăng nhập!", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                mDialog.dismiss();
+                                Toast.makeText(LoginActivity.this, "Tài khoản không tồn tại!", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/duyda/onlinesaleshop/ProductList.java
@@ -0,0 +1,124 @@
+package com.example.duyda.onlinesaleshop;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.duyda.onlinesaleshop.Interface.ItemClickListener;
+import com.example.duyda.onlinesaleshop.Models.Category;
+import com.example.duyda.onlinesaleshop.Models.Product;
+import com.example.duyda.onlinesaleshop.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.squareup.picasso.Picasso;
+
+public class ProductList extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    FirebaseDatabase database;
+    DatabaseReference productList;
+
+
+    FirebaseRecyclerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_list);
+
+        database = FirebaseDatabase.getInstance();
+        productList = database.getReference("Products");
+
+        recyclerView = findViewById(R.id.recycler_product);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        String categoryId = "";
+        if (getIntent() != null)
+            categoryId = getIntent().getStringExtra("CategoryId");
+        if (!categoryId.isEmpty() && categoryId != null) {
+            loadProductList(categoryId);
+        }
+//        loadProductList();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    private void loadProductList(String categoryId) {
+
+        Query query = FirebaseDatabase
+                .getInstance()
+                .getReference("Product")
+                .orderByChild("MenuId")
+                .equalTo(categoryId);
+
+        Log.d("TAGZUY", "" + query.toString());
+        Log.d("TAGZUY1", "" + categoryId.toString());
+
+        FirebaseRecyclerOptions<Product> options =
+                new FirebaseRecyclerOptions.Builder<Product>()
+                        .setQuery(query, Product.class)
+                        .build();
+
+        adapter = new FirebaseRecyclerAdapter<Product, ProductViewHolder>(options) {
+            @Override
+            public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.product_item, parent, false);
+
+                return new ProductViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull Product model) {
+                holder.product_name.setText(model.getName());
+                Picasso.with(getBaseContext()).load(model.getImage())
+                        .into(holder.product_image);
+
+                final Product clickItem = model;
+                holder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+                        Intent productDetail = new Intent(ProductList.this, ProductDetail.class);
+                        productDetail.putExtra("ProductId", adapter.getRef(position).getKey());
+                        startActivity(productDetail);
+                    }
+                });
+            }
+
+        };
+
+        recyclerView.setAdapter(adapter);
+    }
+}
